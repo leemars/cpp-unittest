baeb9fbe77a8547522ce4cba81fa82a5567c9b60
Jun Huang <huangjun@baidu.com>
2012-10-10 16:41:26 +0800

    TDD Kata 2: Step 3 - [REFACTOR] Calcs string from user input

--- ex2/src/string_calculator_app_test.cpp
+++ ex2/src/string_calculator_app_test.cpp
@@ -1,35 +1,51 @@
 #include "string_calculator_app.h"
 
 #include <sstream>
+#include <string>
+#include <vector>
 
 #include "third-party/gmock/gmock.h"
 
-TEST(StringCalculatorAppTest, CalcsStringFrom1stArgument) {
-    std::istringstream input;
-    std::ostringstream output;
-    StringCalculatorApp app(input, output);
+class StringCalculatorAppTest : public ::testing::Test {
+public:
+    StringCalculatorAppTest() : app(input, output) {}
 
-    int argc = 2;
-    char argv1st[] = "scalc";
-    char argv2nd[] = "1,2,3";
-    char *argv[] = {argv1st, argv2nd};
-    app.Run(argc, argv);
-    
+    ~StringCalculatorAppTest() {
+        for (std::vector<char *>::iterator iter = args.begin(); iter != args.end(); ++iter) {
+            delete *iter;
+        }
+    }
+
+    void AddArg(const char *arg) {
+        char *buffer = new char[strlen(arg) + 1];
+        strcpy(buffer, arg);
+        args.push_back(buffer);
+    }
+
+    void Run() {
+        app.Run(args.size(), &args[0]);
+    }
+
+protected:
+    std::stringstream input;
+    std::stringstream output;
+    StringCalculatorApp app;
+    std::vector<char *> args;
+};
+
+TEST_F(StringCalculatorAppTest, CalcsStringFrom1stArgument) {
+    AddArg("scalc");
+    AddArg("1,2,3");
+    Run();
     ASSERT_EQ("The result is 6\n", output.str());
 }
 
-TEST(StringCalculatorAppTest, CalcsStringFromUserInput) {
-    std::stringstream input;
-    std::ostringstream output;
-    StringCalculatorApp app(input, output);
-
-    int argc = 2;
-    char argv1st[] = "scalc";
-    char argv2nd[] = "1,2,3";
-    char *argv[] = {argv1st, argv2nd};
-    input << "1,2,3,4\n1,2,3,4,5\n\n";
-    app.Run(argc, argv);
-
+TEST_F(StringCalculatorAppTest, CalcsStringFromUserInput) {
+    AddArg("scalc");
+    AddArg("1,2,3");
+    input << "1,2,3,4" << std::endl
+        << "1,2,3,4,5" << std::endl
+        << std::endl;
+    Run();
     ASSERT_EQ("The result is 6\nThe result is 10\nThe result is 15\n", output.str());
-
 }
