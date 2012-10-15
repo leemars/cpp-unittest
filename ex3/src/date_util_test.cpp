078abef86a4b02f2b7b40d4505a9b9b2f9b18759
Jun Huang <huangjun@baidu.com>
2012-10-15 16:54:17 +0800

    TDD Kata 3: Step 6 - Refactoring

--- ex3/src/date_util_test.cpp
+++ ex3/src/date_util_test.cpp
@@ -7,17 +7,17 @@ TEST(DateUtilTest, TypicalCommonYearIsNotDivisibleBy4) {
     ASSERT_FALSE(util.IsLeapYear(2001));
 }
 
+TEST(DateUtilTest, AtypicalCommonYearIsDivisibleBy4ButNotDivisibleBy400) {
+    DateUtil util;
+    ASSERT_FALSE(util.IsLeapYear(1900));
+}
+
 TEST(DateUtilTest, TypicalLeapYearIsDivisibleBy4ButNotDivisibleBy100) {
     DateUtil util;
     ASSERT_TRUE(util.IsLeapYear(1996));
 }
 
-TEST(DateUtilTest, AtypicalCommonYearIsDivisibleBy4AndDivisibleBy100ButNotDivisibleBy400) {
-    DateUtil util;
-    ASSERT_FALSE(util.IsLeapYear(1900));
-}
-
-TEST(DateUtilTest, AtypicalLeapYearIsDivisibleBy4AndDivisibleBy400AndDivisibleBy400) {
+TEST(DateUtilTest, AtypicalLeapYearIsDivisibleBy400) {
     DateUtil util;
     ASSERT_TRUE(util.IsLeapYear(2000));
 }
