e520748a4e44d7a28325c4b65a4dc18e375b53b8
Jun Huang <huangjun@baidu.com>
2012-10-15 16:54:17 +0800

    TDD Kata 3: Step 3 - [DEV] Atypical common year is divisible by 4 and divisible by 100 but not divisible by 400

--- ex3/src/date_util.cpp
+++ ex3/src/date_util.cpp
@@ -4,5 +4,8 @@ bool DateUtil::IsLeapYear(int year) {
     if (year % 4 != 0) {
         return false;
     }
+    if (year % 4 == 0 && year % 100 == 0 && year % 400 != 0) {
+        return false;
+    }
     return true;
 }
